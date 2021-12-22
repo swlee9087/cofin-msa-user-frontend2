8b5b7d4d1f9aa082809c3e80df253b4f0959f107
Sw <86825656+swlee9087@users.noreply.github.com>
2021-12-22 17:54:07 +0900

    user pages
    components show but not pages

--- src/user/components/UserLogin.js
+++ src/user/components/UserLogin.js
@@ -16,7 +16,7 @@ import {
     FormControlLabel
   } from '@mui/material';
 import { LoadingButton } from '@mui/lab';
-import UserLost from 'user';
+import UserLost from './UserLost';
 import Navigation from 'common/Navigation';
 
 export default function UserLogin() {
@@ -103,53 +103,5 @@ export default function UserLogin() {
       </Form>
     </FormikProvider>
     )
-//   const handleChange = e => {
-//     const {value, name} = e.target
-//     setLogin({
-//       ...login,
-//       [name] : value
-//     })
-//   }
 
-//   const changeNull = ls =>{
-//     for(const i of ls ){
-//       document.getElementById(i).value = ''
-//     }
-//   }
-//   const handleClick = e => {
-//     e.preventDefault()
-//     e.stopPropagation()
-//     const loginRequest = {username, password}
-    /*
-    userLogin(loginRequest)
-    .then(res => {
-      const user = res.data;
-      if(user.username != null){
-        alert('로그인 성공, '+JSON.stringify(res.data))
-        localStorage.setItem('sessionUser', JSON.stringify(res.data))
-        history.push("/users/detail")
-      }else{
-        alert('아이디, 비번 오류로 로그인 실패  ')
-        changeNull(['username','password'])
-      }
-    })
-    .catch(err => {
-      alert('접속 실패' + err)
-      changeNull(['username','password'])
-    })
-    */
-//   }
- 
-//   return (
-//     <form method="POST">
-//     <ul>
-//         <li><label for="id">아이디</label>
-//         <input type="text" id="username" 
-//             name='username' value={username} onChange={handleChange}/></li>
-//         <li><label for="pw">비밀번호</label>
-//         <input type="password" id="password" name="password" onChange={handleChange}/></li>
-//         <li><input type="button" title="로그인" value="로그인" onClick={handleClick}/></li>
-//     </ul>
-// </form>
-//   );
 }
